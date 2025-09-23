e14cfd8d9be104266ece07ce6394551c915955fe
Justin <1360989+parrajustin@users.noreply.github.com>
2025-09-23 07:22:21 +0000

    fix: add missing plugins

--- src/fixed_plugins.ts
+++ src/fixed_plugins.ts
@@ -17,6 +17,24 @@ export const FIXED_PLUGINS: FixedPlugins[] = [
         repo: "parrajustin/obsidian42-brat",
         localName: "obsidian42-brat",
         desc: "Adds a util to do auto updating of plugins."
+    },
+    {
+        name: "(Work) Person Image Renderer",
+        repo: "parrajustin/obsidian-person-image",
+        localName: "obsidian-person-image",
+        desc: "Adds a preview of a person base64 url encoded image in note prop: 'Profile Picture'."
+    },
+    {
+        name: "Advanced Command URI",
+        repo: "parrajustin/obsidian-advanced-uri",
+        localName: "obsidian-advanced-uri",
+        desc: "Control obsidian through command uris."
+    },
+    {
+        name: "Ledger",
+        repo: "parrajustin/obsidian-finance",
+        localName: "ledger-obsidian",
+        desc: "Plain text accounting."
     }
 ];
 export const FIXED_PLUGIN_REPOS = new Set(FIXED_PLUGINS.map((x) => x.repo));
